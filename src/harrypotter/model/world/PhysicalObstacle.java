c54eabd51b453903b792887ff51f5500905d7f32
Amr M. Kayid <amrkayid2027@gmail.com>
2017-09-19 00:24:34 +0200

    Created (PhysicalObstacle) Subclass

---
+++ src/harrypotter/model/world/PhysicalObstacle.java
@@ -0,0 +1,18 @@
+/*
+ * Represents a physical obstacle blocking the champion’s path.
+ */
+
+package harrypotter.model.world;
+
+public class PhysicalObstacle extends Obstacle {
+
+	/*
+	 * Constructors
+	 */
+	public PhysicalObstacle(int hp) {
+
+		super(hp);
+
+	}
+
+}
