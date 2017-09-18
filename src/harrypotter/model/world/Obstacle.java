a475b100d65ec84c3f6b429ffa729d4ac23a5f35
Amr M. Kayid <amrkayid2027@gmail.com>
2017-09-19 00:23:37 +0200

    Created (Obstacle) Abstract Class

---
+++ src/harrypotter/model/world/Obstacle.java
@@ -0,0 +1,39 @@
+/*
+ * A Superclass representing an obstacle within the map 
+ * that is preventing the contestants from passing through. 
+ * No objects of type Obstacle can be instantiated.
+ */
+
+package harrypotter.model.world;
+
+public abstract class Obstacle {
+
+	/*
+	 * Attributes
+	 */
+	/*
+	 * It represents how much damage the obstacle can withstand.
+	 */
+	private int hp;	//READ & WRITE
+
+	/*
+	 * Constructors
+	 */
+	public Obstacle(int hp) {
+
+		this.hp = hp;
+
+	}
+
+	//---------------------- Getter && Setter Methods ----------------------//
+	public int getHp() {
+
+		return hp;
+
+	}
+	
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	//#####################################################################//
+}
