a5c544efb2134854eb9ddcbe53135a7a9e27aed6
Amr M. Kayid <amrkayid2027@gmail.com>
2017-09-19 00:41:30 +0200

    Created Champion Interface

---
+++ src/harrypotter/model/character/Champion.java
@@ -0,0 +1,14 @@
+/*
+ * Interface containing the methods available to wizards who participate in the tournament
+ */
+
+package harrypotter.model.character;
+
+public interface Champion {
+	
+	/*
+	 * Methods
+	 */
+	public void useTrait();
+
+}
