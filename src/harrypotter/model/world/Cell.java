7bca35bafbc7b02fcd70dfa114a2e9ff532271af
Amr M. Kayid <amrkayid2027@gmail.com>
2017-09-19 00:27:22 +0200

    Created (Cell) Abstract Class

---
+++ src/harrypotter/model/world/Cell.java
@@ -0,0 +1,10 @@
+/*
+ * A Superclass representing a single cell in the map 
+ * that any character can pass through. 
+ * No objects of type Cell can be instantiated.
+ */
+package harrypotter.model.world;
+
+public abstract class Cell {
+
+}
