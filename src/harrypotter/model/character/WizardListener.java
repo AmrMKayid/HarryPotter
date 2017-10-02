18f747568f700b544d5906d0efca3b6fa97427d7
Amr M. Kayid <amrkayid2027@gmail.com>
2017-10-02 13:31:44 +0200

    Added WizardListener Interface

---
+++ src/harrypotter/model/character/WizardListener.java
@@ -0,0 +1,18 @@
+/*
+ * Interface containing the methods needed for the wizard listeners
+ */
+package harrypotter.model.character;
+
+import harrypotter.model.world.Direction;
+
+public interface WizardListener {
+
+	public void onGryffindorTrait();
+
+	public void onSlytherinTrait(Direction d);
+
+	public void onHufflepuffTrait();
+
+	public Object onRavenclawTrait();
+
+}
